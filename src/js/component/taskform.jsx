e67930599b97761a3fd283619643e4899dc07183
luisgomez7t <162279891+7tCam@users.noreply.github.com>
2024-10-02 18:58:32 +0000

    todo list con fetch terminado

---
+++ src/js/component/taskform.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const TaskForm = ({ addTask }) => {
+    const [task, setTask] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (task.trim()) {
+            addTask({ label: task, done: false });
+            setTask('');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                placeholder="Agrega nueva tarea"
+                className='text-input'
+            />
+
+        </form>
+    );
+};
+
+export default TaskForm;
+
